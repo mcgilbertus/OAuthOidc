a095b2a467b4ac9c467c91c178a20635a9a7eb62
Ernesto Cullen <ernestocullen@gmail.com>
2024-01-14 16:24:50 -0500

    Initial commit

---
+++ GetDiscoveryDoc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Nodes;
+
+const string serverUrl = "https://demo.duendesoftware.com";
+const string discoveryUrl = ".well-known/openid-configuration";
+
+// 1. Using httpClient
+var httpClient = new HttpClient();
+var discoveryStr = await httpClient.GetStringAsync($"{serverUrl}/{discoveryUrl}");
+var discoveryDoc = JsonNode.Parse(discoveryStr);
+Console.WriteLine(discoveryDoc?.ToString());
+
+var tokenUrl = discoveryDoc["token_endpoint"]?.ToString();
+Console.WriteLine($"Token endpoint: {tokenUrl}");
---
+++ UsingIdentityModel/Program.cs
@@ -0,0 +1,13 @@
+﻿using IdentityModel.Client;
+
+const string serverUrl = "https://demo.duendesoftware.com";
+const string discoveryUrl = ".well-known/openid-configuration";
+
+var client = new HttpClient();
+
+var discoveryDoc = await client.GetDiscoveryDocumentAsync($"{serverUrl}/{discoveryUrl}");
+if (discoveryDoc.IsError)
+    throw new ApplicationException(discoveryDoc.Error);
+
+Console.WriteLine($"Discovery doc: {discoveryDoc.Json}");
+    
