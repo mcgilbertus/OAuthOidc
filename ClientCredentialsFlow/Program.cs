8bf8c890c9f6505bdbbf17edeb674c845d0a3a9a
Ernesto Cullen <ernestocullen@gmail.com>
2024-01-18 21:23:52 -0500

    Removed 'UsingIdentityModel' project; rolled up into 'GetDiscoveryDoc'

---
+++ ClientCredentialsFlow/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using IdentityModel.Client;
+
+/// Get an access token from the identity server using ClientCredentials flow
+
+const string serverUrl = "https://demo.duendesoftware.com";
+const string discoveryUrl = ".well-known/openid-configuration";
+
+Console.WriteLine("Using IdentityModel.Client");
+var httpClient = new HttpClient();
+await PrintTokenIdentityModel(httpClient);
+Console.WriteLine();
+
+Console.WriteLine("Using vanilla HttpClient");
+await PrintTokenHttpClient(httpClient);
+Console.WriteLine();
+
+return;
+
+
+async Task PrintTokenIdentityModel(HttpClient client)
+{
+    // using IdentityModel
+    var discoveryDoc = await client.GetDiscoveryDocumentAsync($"{serverUrl}/{discoveryUrl}");
+    if (discoveryDoc.IsError)
+        throw new ApplicationException(discoveryDoc.Error);
+
+    var tokenEndpoint = discoveryDoc.TokenEndpoint;
+    Console.WriteLine($"Token endpoint: {tokenEndpoint}");
+    var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+        new ClientCredentialsTokenRequest()
+        {
+            Address = tokenEndpoint,
+            ClientId = "m2m",
+            ClientSecret = "secret",
+            Scope = "api"
+        });
+
+    var tokenJson = JsonSerializer.Serialize(tokenResponse.Json, new JsonSerializerOptions { WriteIndented = true });
+    Console.WriteLine($"Token: {tokenJson}");
+}
+
+async Task PrintTokenHttpClient(HttpClient client)
+{
+    // using vanilla HttpClient
+    var discoveryStr = await client.GetStringAsync($"{serverUrl}/{discoveryUrl}");
+    var discoveryDoc = JsonNode.Parse(discoveryStr);
+
+    var tokenUrl = discoveryDoc!["token_endpoint"]?.ToString();
+    var parameters = new Dictionary<string, string>
+    {
+        { "address", tokenUrl },
+        { "client_id", "m2m" },
+        { "client_secret", "secret" },
+        { "grant_type", "client_credentials" },
+        { "scope", "api" }
+    };
+    
+    using var request = new HttpRequestMessage(HttpMethod.Post, tokenUrl);
+    request.Headers.Add("Accept", "application/json");
+    request.Content = new FormUrlEncodedContent(parameters);
+    var tokenResponse = await client.SendAsync(request);
+    var tokenStr = await tokenResponse.Content.ReadAsStringAsync();
+    
+    var tokenJson = JsonSerializer.Serialize(JsonNode.Parse(tokenStr), new JsonSerializerOptions { WriteIndented = true });
+    Console.WriteLine($"Token: {tokenJson}");
+}
--- GetDiscoveryDoc/Program.cs
+++ GetDiscoveryDoc/Program.cs
@@ -1,13 +1,41 @@
 ﻿using System.Text.Json.Nodes;
+using IdentityModel.Client;
 
 const string serverUrl = "https://demo.duendesoftware.com";
 const string discoveryUrl = ".well-known/openid-configuration";
 
-// Get discovery document using vanilla HttpClient
+Console.WriteLine("Using IdentityModel.Client");
 var httpClient = new HttpClient();
-var discoveryStr = await httpClient.GetStringAsync($"{serverUrl}/{discoveryUrl}");
-var discoveryDoc = JsonNode.Parse(discoveryStr);
-Console.WriteLine(discoveryDoc?.ToString());
+await GetDiscoveryDocIdentityModelAsync(httpClient);
+Console.WriteLine();
 
-var tokenUrl = discoveryDoc["token_endpoint"]?.ToString();
-Console.WriteLine($"Token endpoint: {tokenUrl}");
+Console.WriteLine("Using vanilla HttpClient");
+await GetDiscoveryDocHttpClientAsync(httpClient);
+Console.WriteLine();
+
+return;
+
+async Task GetDiscoveryDocIdentityModelAsync(HttpClient httpClient)
+{
+    var discoveryDoc = await httpClient.GetDiscoveryDocumentAsync($"{serverUrl}/{discoveryUrl}");
+    if (discoveryDoc.IsError)
+        throw new ApplicationException(discoveryDoc.Error);
+
+    Console.WriteLine($"Discovery doc: {discoveryDoc.Json}");
+    Console.WriteLine();
+    // as an example, let's print the authentication flows supported
+    var grantTypes = discoveryDoc.TryGetString("grant_types_supported");
+    Console.WriteLine($"Authentication flows supported: {string.Join(",",grantTypes)}");
+
+}
+
+async Task GetDiscoveryDocHttpClientAsync(HttpClient httpClient)
+{
+    // Get discovery document using vanilla HttpClient
+    var discoveryStr = await httpClient.GetStringAsync($"{serverUrl}/{discoveryUrl}");
+    var discoveryDoc = JsonNode.Parse(discoveryStr);
+    Console.WriteLine(discoveryDoc?.ToString());
+    // as an example, let's print the token endpoint
+    var tokenUrl = discoveryDoc["token_endpoint"]?.ToString();
+    Console.WriteLine($"Token endpoint: {tokenUrl}");
+}
